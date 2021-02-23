d05b714afc43896b9fb877a73e44416845e17a25
csnascimento <cs_nascimento@yahoo.com.br>
2021-02-23 00:11:47 -0300

    38 - Introdução a POO

--- index.js
+++ index.js
@@ -22,9 +22,35 @@ var user = {};
  });
 */
 
+function addLine(dataUser){
 
- document.getElementById("form-user-create").addEventListener("submit", function(event){
+    console.log(dataUser);
+
+    var tr = document.createElement("tr");
+
+    document.getElementById("table-users").innerHTML = `
+    
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    
+    `;        
+
+}
+
+
+    document.getElementById("form-user-create").addEventListener("submit", function(event){
+
     event.preventDefault();  // cancela a ação padrão do voluntário que seria atalizar
+
     fields.forEach(function (field, index) {
 
         if (field.name == "gender") {
@@ -37,7 +63,19 @@ var user = {};
         
      });
 
-     console.log(user);
+     var objUser = new User(
+                            user.name, 
+                            user.gender, 
+                            user.birth, 
+                            user.coutry,
+                            user.email,
+                            user.password, 
+                            user.photo, 
+                            user.admin
+                            )
+
+     addLine(objUser);
+
  });
 
 
